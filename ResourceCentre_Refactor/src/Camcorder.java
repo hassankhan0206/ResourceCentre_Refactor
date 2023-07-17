d7ff9ed7c41ee42f147c4974a6b047a421dceb3e
joremii <22022505@rp.edu.sg>
2023-07-17 15:05:33 +0800

    public String updated

--- ResourceCentre_Refactor/src/Camcorder.java
+++ ResourceCentre_Refactor/src/Camcorder.java
@@ -1,20 +1,17 @@
-
 public class Camcorder extends Item{
-	private int opticalZoom;
+  private int opticalZoom;
 
-	public Camcorder(String assetTag, String description, int opticalZoom) {
-		super(assetTag, description);
-		this.opticalZoom = opticalZoom;
-	}
+  public Camcorder(String assetTag, String description, int opticalZoom) {
+    super(assetTag, description);
+    this.opticalZoom = opticalZoom;
+  }
 
-	public int getOpticalZoom() {
-		return opticalZoom;
-	}
-	
-	public String toString(){
-		String output = "";
-		// Write your codes here
-		return output;
-	}
+  public int getOpticalZoom() {
+    return opticalZoom;
+  }
+  
+  public String toString(){
+    String output = super.toString();
+    return String.format("%-63s -%20s", output, opticalZoom);
+  }
 }
-
